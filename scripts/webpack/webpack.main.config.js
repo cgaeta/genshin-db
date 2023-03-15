ef361cecec8220b2e736d3fbcc11242b856f08b4
Chris Gaeta <chrismgaeta@gmail.com>
2023-03-15 14:14:54 -0400

    fix outdir

--- scripts/webpack/webpack.main.config.js
+++ scripts/webpack/webpack.main.config.js
@@ -2,29 +2,37 @@ const path = require('path');
 
 // Call this from the project root.
 
-module.exports = env => ({
-	entry: './src/main.js',
-	// target: 'web', // not needed
-	output: {
-		path: env.outdir ? env.outdir : path.resolve(__dirname, '../../dist'),
-		filename: typeof env.filename === 'string' ? (env.filename + (env.filename.endsWith('.js') ? '' : '.js')) : 'genshindb.js',
+module.exports = (env) => {
+  return {
+    entry: './src/main.js',
+    // target: 'web', // not needed
+    output: {
+      path: env.outdir ? env.outdir : path.resolve(__dirname, '../../dist'),
+      filename:
+        typeof env.filename === 'string'
+          ? env.filename + (env.filename.endsWith('.js') ? '' : '.js')
+          : 'genshindb.js',
 
-		library: env.libraryname ? env.libraryname : 'GenshinDb',
-		libraryTarget: 'umd',
-		umdNamedDefine: true,
-		globalObject: `(typeof self !== 'undefined' ? self : this)`
-
-	},
-	resolve: {
-		alias: {
-			[path.resolve(__dirname, '../../src/min/data.min.json')]: path.resolve(__dirname, '../../src/min/data.min.json.gzip')
-		}
-	},
-	module: {
-		rules: [{
-			test: /\.gzip$/,
-			use: 'arraybuffer-loader'
-		}]
-	}
-
-});
+      library: 'GenshinDb',
+      libraryTarget: 'umd',
+      umdNamedDefine: true,
+      globalObject: `(typeof self !== 'undefined' ? self : this)`,
+    },
+    resolve: {
+      alias: {
+        [path.resolve(__dirname, '../../src/min/data.min.json')]: env.outdir
+          ? path.resolve(env.outdir, './data.min.json.gzip')
+          : path.resolve(__dirname, '../../dist/min/data.min.json.gzip'),
+      },
+      aliasFields: [],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.gzip$/,
+          use: 'arraybuffer-loader',
+        },
+      ],
+    },
+  };
+};
